a867d6ef3f28005be31f0b736b3c2c03a4e7e926
leesiri1004 <leesiri1004@gmail.com>
2020-12-07 17:57:24 +0900

    2020/12/07 수업예제 및 책 예제 등록

---
+++ Ch05/lib/calc.js
@@ -0,0 +1,18 @@
+// js -> javascript file
+// 그래서 <script></script> 선언을 안함
+
+function plus(x, y) {
+    return x+y;
+}
+
+function minus(x, y) {
+    return x-y;
+}
+
+function multi(x, y) {
+    return x*y;
+}
+
+function div(x, y) {
+    return x/y;
+}
